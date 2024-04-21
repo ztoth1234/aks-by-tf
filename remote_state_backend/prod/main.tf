a93bc62e84c62f43214477ff68694a3ee90634a3
Zoltan Toth <ztoth123@gmail.com>
2024-04-21 10:58:11 +0200

    environment from prod plan removed

--- remote_state_backend/prod/main.tf
+++ remote_state_backend/prod/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "tfstate" {
+resource "azurerm_resource_group" "rg-tfstate" {
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
@@ -7,8 +7,8 @@ resource "azurerm_resource_group" "tfstate" {
 resource "azurerm_storage_account" "tfstate" {
 
   name                            = var.storage_account_name
-  resource_group_name             = azurerm_resource_group.tfstate.name
-  location                        = azurerm_resource_group.tfstate.location
+  resource_group_name             = azurerm_resource_group.rg-tfstate.name
+  location                        = azurerm_resource_group.rg-tfstate.location
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
