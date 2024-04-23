851a25064e5df3c660194cc81efaca33849c4c3c
Zoltan Toth <ztoth123@gmail.com>
2024-04-23 11:58:04 +0200

    modified outputs

--- nonprod/outputs.tf
+++ nonprod/outputs.tf
@@ -1,6 +1,6 @@
 output "resource_group_ids" {
   description = ""
   value = {
-    for k, v in var.resource_groups : k => v.id
+    for k, v in azurerm_resource_group.nonprod_rgs : k => v.id
   }
 }
